8d9c549c86a868aecceb490dd76b279f4c09f6dc
Arthur Senedez Micheloni <arthursemic@outlook.com>
2022-06-23 08:21:44 -0300

    first commit

---
+++ exer.js
@@ -0,0 +1,21 @@
+var meuCarro = {
+    portas:4,
+    nome:"monza",
+}
+
+console.log("Meu carro se chama "+meuCarro.nome+" ele tem "+meuCarro.portas+ " portas!")
+
+var botao = document.querySelector("button[name='salvar']")
+var lista = document.querySelector('ul')
+
+
+botao.addEventListener("click", function(){
+    var titulo = document.querySelector("h2[name='ler']")
+    var span = document.querySelector("span[name='editar']")
+
+    lista.insertAdjacentHTML("afterbegin",'<li>'+span.innerHTML+'</li>')
+
+    titulo.innerHTML = span.innerHTML
+
+    })
+
